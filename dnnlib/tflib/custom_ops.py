5a9fdc809ac6fe1dec423fa5831544fedca5689e
mazhenli <mzlzjhz@163.com>
2024-03-17 11:08:06 +0800

    test

--- dnnlib/tflib/custom_ops.py
+++ dnnlib/tflib/custom_ops.py
@@ -133,7 +133,8 @@ def get_plugin(cuda_file):
 
         # Hash build configuration.
         md5.update(('nvcc_cmd: ' + nvcc_cmd).encode('utf-8') + b'\n')
-        md5.update(('tf.VERSION: ' + tf.VERSION).encode('utf-8') + b'\n')
+        #md5.update(('tf.VERSION: ' + tf.VERSION).encode('utf-8') + b'\n')
+        md5.update(('2.15.0: ' + '2.15.0').encode('utf-8') + b'\n')
         md5.update(('cuda_cache_version_tag: ' + cuda_cache_version_tag).encode('utf-8') + b'\n')
 
         # Compile if not already compiled.
